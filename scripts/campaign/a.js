a1f94f5a2276d9b0769028b5874541364a7d9279
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2020-12-21 12:22:54 +0300

    Add files via upload

---
+++ scripts/campaign/a.js
@@ -0,0 +1,54 @@
+Events.on(ClientLoadEvent, () => {
+
+    var arrs = [
+        [Blocks.water, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.stone, Blocks.stone],
+        [Blocks.water, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.stone, Blocks.stone, Blocks.stone],
+        [Blocks.water, Blocks.grass, Blocks.grass, Blocks.sand, Blocks.salt, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.stone, Blocks.stone, Blocks.stone],
+        [Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.salt, Blocks.salt, Blocks.salt, Blocks.sand, Blocks.stone, Blocks.stone, Blocks.stone, Blocks.snow, Blocks.iceSnow, Blocks.ice],
+        [Blocks.deepwater, Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.salt, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice],
+        [Blocks.deepwater, Blocks.water, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.iceSnow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.snow, Blocks.ice],
+        [Blocks.deepwater, Blocks.sandWater, Blocks.sand, Blocks.sand, Blocks.grass, Blocks.grass, Blocks.snow, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.ice, Blocks.snow, Blocks.ice],
+        [Blocks.water, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.hotrock, Blocks.grass, Blocks.ice, Blocks.snow, Blocks.ice, Blocks.ice],
+        [Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.dirt, Blocks.snow, Blocks.grass, Blocks.grass, Blocks.ice, Blocks.snow, Blocks.ice, Blocks.ice],
+        [Blocks.grass, Blocks.grass, Blocks.grass, Blocks.dirt, Blocks.ice, Blocks.ice, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice],
+        [Blocks.water, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.grass, Blocks.ice, Blocks.ice, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice],
+        [Blocks.grass, Blocks.grass, Blocks.grass, Blocks.dirt, Blocks.grass, Blocks.dirt, Blocks.iceSnow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice],
+        [Blocks.grass, Blocks.grass, Blocks.snow, Blocks.ice, Blocks.iceSnow, Blocks.snow, Blocks.snow, Blocks.snow, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice, Blocks.ice]
+    ];
+    const hhhGenerator = extend(SerpuloPlanetGenerator, {
+        getBlock(p){
+            this.arr = arrs;
+            this.super$getBlock(p);
+        }
+    });
+    hhhGenerator.arr = arrs;
+
+
+
+    const ananyum = new Planet("ananyum", Planets.sun, 2, 1);
+    ananyum.generator = hhhGenerator;
+    ananyum.mesh = new HexMesh(ananyum, 3);
+    ananyum.orbitRadius = 5;
+    ananyum.orbitTime = 1.5 * 60;
+    ananyum.rotateTime = 30;
+    ananyum.bloom = true;
+    ananyum.accessible = true;
+    ananyum.startSector = 1;
+    ananyum.hasAtmosphere = true;
+    ananyum.atmosphereColor = Liquids.oil.color;
+    ananyum.atmosphereRadIn = 0.1;
+    ananyum.atmosphereRadOut = 0.5;
+    ananyum.alwaysUnlocked = true;
+    ananyum.localizedName = "Yakko";
+
+
+
+
+    const antarcticaSurvival = new SectorPreset("fortress", ananyum, 1);
+    antarcticaSurvival.captureWave = 30;
+    antarcticaSurvival.localizedName = "Fortress";
+    antarcticaSurvival.difficulty = 6;
+    antarcticaSurvival.alwaysUnlocked = true;
+
+
+});
---
+++ scripts/campaign/planet.js
@@ -0,0 +1,54 @@
+Events.on(ClientLoadEvent, () => {
+
+    var arrs = [
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.hotrock, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand],
+        [Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand, Blocks.darksand]
+    ];
+    const hhhGenerator = extend(SerpuloPlanetGenerator, {
+        getBlock(p){
+            this.arr = arrs;
+            this.super$getBlock(p);
+        }
+    });
+    hhhGenerator.arr = arrs;
+
+
+
+    const tontontros = new Planet("tontontros", Planets.sun, 1, 1);
+    tontontros.generator = hhhGenerator;
+    tontontros.mesh = new HexMesh(tontontros, 5);
+    tontontros.orbitRadius = 10;
+    tontontros.orbitTime = 1.5 * 60;
+    tontontros.rotateTime = 30;
+    tontontros.bloom = true;
+    tontontros.accessible = true;
+    tontontros.startSector = 1;
+    tontontros.hasAtmosphere = true;
+    tontontros.atmosphereColor = Liquids.slag.color;
+    tontontros.atmosphereRadIn = 0.1;
+    tontontros.atmosphereRadOut = 0.3;
+    tontontros.alwaysUnlocked = true;
+    tontontros.localizedName = "Mon";
+
+
+
+
+    const antarcticaSurvival = new SectorPreset("fork", tontontros, 1);
+    antarcticaSurvival.captureWave = 30;
+    antarcticaSurvival.localizedName = "Fork";
+    antarcticaSurvival.difficulty = 6;
+    antarcticaSurvival.alwaysUnlocked = true;
+
+
+});
